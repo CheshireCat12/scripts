59e858dedccf914847d40a0dc6fe68f29c1448ba
CheshireCat12 <anthony.gillioz@outlook.com>
2022-09-21 11:40:01 +0200

    modify the script

--- baseline/script_baseline.sh
+++ baseline/script_baseline.sh
@@ -6,9 +6,8 @@
 #SBATCH --mem-per-cpu=20G
 #SBATCH --cpus-per-task=2
 #SBATCH --time=0-10:00:00
-#SBATCH --output=~/neo_slurms/baseline_%A_%a.out
-#SBATCH --array=1-17
-
+#SBATCH --output=/storage/homefs/ag21k209/neo_slurms/baseline_%A_%a.out
+#SBATCH --array=7
 
 param_store=./arguments_baseline.txt
 
@@ -17,9 +16,10 @@ dataset_name=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {prin
 dataset_save=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}')
 
 # Put your code below this line
+module load Workspace_Home
 module load Python/3.9.5-GCCcore-10.3.0.lua
 cd $HOME/graph_library/graph_reduction/baseline/
 source venv/bin/activate
 
-srun python baseline.py --dataset $dataset_name --folder_results $HOME/graph_library/data/$dataset_save/baseline/
+srun python baseline.py --dataset $dataset_name --root_dataset $SCRATCH/tmp/ --folder_results $SCRATCH/data/$dataset_save/baseline/ -v
 
