37a5a82d553a1225f6c58f967bcf3a8c0e0117d1
CheshireCat12 <anthony.gillioz@outlook.com>
2022-10-03 11:32:12 +0200

    add timing scripts

--- graph_classification_ged/timing/script_ged_on_baseline.sh
+++ graph_classification_ged/timing/script_ged_on_baseline.sh
@@ -3,11 +3,11 @@
 #SBATCH --mail-user=anthony.gillioz@inf.unibe.ch
 #SBATCH --mail-type=end,fail
 
-#SBATCH --mem-per-cpu=30G
-#SBATCH --cpus-per-task=8
-#SBATCH --time=0-5:00:00
+#SBATCH --mem-per-cpu=40G
+#SBATCH --cpus-per-task=10
+#SBATCH --time=2-23:00:00
 #SBATCH --output=/storage/homefs/ag21k209/neo_slurms/classification_ged_on_baseline_%A_%a.out
-#SBATCH --array=2
+#SBATCH --array=1-17
 
 param_store=./arguments_ged_on_baseline_full.txt
 
@@ -21,5 +21,5 @@ module load Python/3.9.5-GCCcore-10.3.0.lua
 cd $HOME/graph_library/graph_classification/graph-classification-ged/
 source venv/bin/activate
 
-srun python main.py --root_dataset $SCRATCH/data/$dataset_save/baseline --n_cores 8 --save_gt_labels --save_predictions --save_distances --folder_results $HOME/graph_library/results/ged_classification/$dataset_save/baseline -v
+srun python main.py --root_dataset $SCRATCH/data/$dataset_save/baseline --n_cores 8 --save_predictions --save_distances --folder_results $HOME/graph_library/results/ged_classification/$dataset_save/baseline -v
 
--- graph_classification_ged/timing/script_ged_timing.sh
+++ graph_classification_ged/timing/script_ged_timing.sh
@@ -3,13 +3,13 @@
 #SBATCH --mail-user=anthony.gillioz@inf.unibe.ch
 #SBATCH --mail-type=end,fail
 
-#SBATCH --mem-per-cpu=30G
-#SBATCH --cpus-per-task=8
-#SBATCH --time=0-5:00:00
-#SBATCH --output=/storage/homefs/ag21k209/neo_slurms/classification_ged_on_baseline_%A_%a.out
-#SBATCH --array=2
+#SBATCH --mem-per-cpu=40G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-23:00:00
+#SBATCH --output=/storage/homefs/ag21k209/neo_slurms/timing_ged_on_baseline_%A_%a.out
+#SBATCH --array=1-2
 
-param_store=./arguments_ged_on_baseline_full.txt
+param_store=./arguments_ged_timing.txt
 
 # Get first argument
 dataset_name=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
@@ -21,5 +21,5 @@ module load Python/3.9.5-GCCcore-10.3.0.lua
 cd $HOME/graph_library/graph_classification/graph-classification-ged/
 source venv/bin/activate
 
-srun python main.py --root_dataset $SCRATCH/data/$dataset_save/baseline --n_cores 8 --save_gt_labels --save_predictions --save_distances --folder_results $HOME/graph_library/results/ged_classification/$dataset_save/baseline -v
+srun python main.py --root_dataset $SCRATCH/data/$dataset_save/baseline --n_cores 0 --ks 3 --alphas 0.5 --folder_results $HOME/graph_library/results/ged_timing/$dataset_save/baseline -v
 
