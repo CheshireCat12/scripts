532aa4e804b4e1b415156eba02766dc0bc542664
CheshireCat12 <anthony.gillioz@outlook.com>
2022-10-14 15:42:01 +0200

    modify ged baseline script

--- graph_classification/graph_classification_ged/spectral/script_ged_on_spectral_small.sh
+++ graph_classification/graph_classification_ged/spectral/script_ged_on_spectral_small.sh
@@ -3,13 +3,14 @@
 #SBATCH --mail-user=anthony.gillioz@inf.unibe.ch
 #SBATCH --mail-type=end,fail
 
-#SBATCH --mem-per-cpu=30G
-#SBATCH --cpus-per-task=10
-#SBATCH --time=2-23:00:00
-#SBATCH --output=/storage/homefs/ag21k209/neo_slurms/classification_ged_on_spectral_%A_%a.out
-#SBATCH --array=1-680
+#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=epyc2
+#SBATCH --time=0-03:00:00
+#SBATCH --output=/storage/homefs/ag21k209/neo_slurms/classification_ged_on_spectral_small_%A_%a.out
+#SBATCH --array=1-200
 
-param_store=./arguments_ged_on_spectral_full.txt
+param_store=./arguments_ged_on_spectral_small.txt
 
 # Get first argument
 dataset_name=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
@@ -25,5 +26,11 @@ module load Python/3.9.5-GCCcore-10.3.0.lua
 cd $HOME/graph_library/graph_classification/graph-classification-ged/
 source venv/bin/activate
 
-srun python main.py --root_dataset $SCRATCH/data/$dataset_save/spectral/$clustering_algo/$merging_method/red_fact$reduction/dimensions$dimension --n_cores 8 --save_predictions --save_distances --folder_results $HOME/graph_library/results/ged_classification/$dataset_save/spectral/$clustering_algo/$merging_method/red_fact$reduction/dimensions$dimension -v
+# !!!!!!!!
+# Don't forget to add 1 in the list of seeds
+# !!!!!!!!!!!
 
+for seed in 183 929 1195 2489
+do
+   srun python main.py --root_dataset $SCRATCH/data/$dataset_save/spectral/$clustering_algo/$merging_method/red_fact$reduction/dimensions$dimension --n_cores 8 --seed $seed --save_gt_labels --save_predictions --save_distances --folder_results $HOME/graph_library/results/ged_classification/$dataset_save/spectral/$seed/$clustering_algo/$merging_method/red_fact$reduction/dimensions$dimension -v
+done
