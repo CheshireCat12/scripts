900e3c15bbb8d3e7f6c7a79fee6f76567880b391
CheshireCat12 <anthony.gillioz@outlook.com>
2022-10-14 09:38:03 +0200

    modify script small to run with short qos

---
+++ graph_classification/graph_classification_ged/baseline/script_ged_on_baseline_small.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --mail-user=anthony.gillioz@inf.unibe.ch
+#SBATCH --mail-type=end,fail
+
+#SBATCH --mem-per-cpu=10G
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=epyc2
+#SBATCH --qos=job_epyc2_short
+#SBATCH --time=0-5:00:00
+#SBATCH --output=/storage/homefs/ag21k209/neo_slurms/classification_ged_on_baseline_small_%A_%a.out
+#SBATCH --array=1-5
+
+param_store=./arguments_ged_on_baseline_small.txt
+
+# Get first argument
+dataset_name=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
+dataset_save=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}')
+
+# Put your code below this line
+module load Workspace_Home
+module load Python/3.9.5-GCCcore-10.3.0.lua
+cd $HOME/graph_library/graph_classification/graph-classification-ged/
+source venv/bin/activate
+
+for i in 183 929 1195 2489 2764
+do
+   srun python main.py --root_dataset $SCRATCH/data/$dataset_save/baseline --n_cores 8 --seed $i --save_gt_labels --save_predictions --save_distances --folder_results $HOME/graph_library/results/ged_classification/$dataset_save/baseline$i -v
+done
