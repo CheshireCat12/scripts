4da672e405fcfc8d4779f45156a225ca40cc0f3c
CheshireCat12 <anthony.gillioz@outlook.com>
2023-10-16 15:44:21 +0200

    minor change

--- graph_classification/graph_classification_ged/centrality/script_ged_on_centrality_small.sh
+++ graph_classification/graph_classification_ged/centrality/script_ged_on_centrality_small.sh
@@ -23,4 +23,4 @@ module load Python/3.9.5-GCCcore-10.3.0.lua
 cd $HOME/graph_library/graph_classification/graph-classification-ged/
 source venv/bin/activate
 
-srun python main.py --root-dataset $SCRATCH/data/$dataset_name/centrality/$centrality_name/$rho --graph-format pkl --n-trials 10 --n-outer-cv 10 --n-inner-cv 3 --n_cores 10 --save-gt-labels --save-predictions --folder-results $HOME/graph_library/results/ged_classification/$dataset_name/centrality/$centrality_name/$rho -v
+srun python main.py --root-dataset $SCRATCH/data/$dataset_name/centrality/$centrality_name/$rho --graph-format pkl --n-trials 10 --n-outer-cv 10 --n-inner-cv 3 --n-cores 10 --save-gt-labels --save-predictions --folder-results $HOME/graph_library/results/ged_classification/$dataset_name/centrality/$centrality_name/$rho -v
